24932129fefc951de1204dc3da95050a4f2ceb5b
Mohamed Fathy <codetwostart@gmail.com>
2019-10-12 15:12:43 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace forandforeachloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] numbers = {2,3,9,1};
+           //string interpolation
+           /* for(int i = 0 ;i < 4; i++){
+               Console.WriteLine($" item {numbers[i]}");
+           } */
+           foreach(int number in numbers){
+               Console.WriteLine($"Item {number}");
+           }
+           string[] courses = {"C#","ASP.net","PHP","Android"};
+           foreach(string course in courses){
+               Console.WriteLine(course);
+           }
+        }
+    }
+}
